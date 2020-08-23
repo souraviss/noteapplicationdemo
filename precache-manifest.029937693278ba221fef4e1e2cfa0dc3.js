19c1278973a4c522e98b43522171486005f8539e
sourav.iss2015 <sourav.iss2015@gmail.com>
2020-08-23 16:04:48 +0530

    first commit

---
+++ precache-manifest.029937693278ba221fef4e1e2cfa0dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e974a0971597082f47e164d48e19019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcabc6953f4a5eec9d9",
+    "url": "/static/css/main.8195507e.chunk.css"
+  },
+  {
+    "revision": "ab8624a544e79208eb78",
+    "url": "/static/js/2.06727bde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.06727bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcabc6953f4a5eec9d9",
+    "url": "/static/js/main.3ecd3e1d.chunk.js"
+  },
+  {
+    "revision": "541e80a3cd875ac64b17",
+    "url": "/static/js/runtime-main.e49efe14.js"
+  }
+]);
